ac09786970b600e0c792acaff7a480a3d4cdb7d1
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-02-10 15:56:12 +0700

    Added MONGOLAB configuration

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
-var mongoProdUri = process.env.MONGOHQ_URL || 'localhost:27017/pagelogger';
+var mongoProdUri = process.env.MONGOLAB_URI || 'localhost:27017/pagelogger_Prod';
 
 var config = {
 	local: {
 		mode: 'local',
 		port: 3000,
-		mongoUrl: 'localhost:27017/pagelogger_dev'
+		mongoUrl: 'localhost:27017/pagelogger_Dev'
 	},
-	test: {
-		mode: 'test',
+	staging: {
+		mode: 'staging',
 		port: 4000,
-		mongoUrl: 'localhost:27017/pagelogger_test'
+		mongoUrl: 'localhost:27017/pagelogger_Test'
 	},
 	prod: {
 		mode: 'prod',
@@ -20,4 +20,4 @@ var config = {
 
 module.exports = function (mode) {
 	return config[mode || process.argv[2] || 'local'] || config.local;
-};
+}
